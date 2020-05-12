e6bd275017ea35379ef959e26ad6c985cd9cda52
Andrew <koni4iva@gmail.com>
2020-05-13 01:53:06 +0400

    Log in with DB

--- application.py
+++ application.py
@@ -1,15 +1,47 @@
 import os
 import requests
 
-from flask import Flask, jsonify, redirect, render_template
+from flask import g, Flask, jsonify, flash, session, redirect, render_template, request, session
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from functools import wraps
 from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/check", methods=["GET"])
+def check():
+    name_test = db.execute("SELECT email, username FROM users WHERE email = :test_email",
+                           {"test_email":request.args.get('email')}).fetchone()
+    if name_test:
+        return jsonify({ "success": False, "username": name_test['username'], "email": name_test['email'] })
+    else:
+        return jsonify({ "success": True } )
+
+@app.route("/change_name", methods=["GET"])
+def change_name():
+    if db.execute("UPDATE users SET username = :new_name WHERE email = :email",
+                           {"new_name":request.args.get('new_user_name'),
+                            "email"   :request.args.get('email')}):
+	    db.commit()
+	    return jsonify({ "success": True, "username": request.args.get('new_user_name'), "email": request.args.get('email')})
+    else:
+        return jsonify({ "success": False } )
--- static/quire.js
+++ static/quire.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //изменение фона кнопок каналов при наведении мышки
     document.querySelectorAll('.chanel_name').forEach(function(chanel_block) {
       chanel_block.onmouseover = function() {
         chanel_block.classList.remove('bg-light');
@@ -12,3 +13,24 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+function message_form_resize(){
+  document.getElementById('send_message_form').style.width = document.querySelector('.message_block').clientWidth +'px';
+  // надо добавить проверку не выше ли левый угол нижней границы последнего элемента сообщений
+  document.getElementById('send_message_form').style.top = window.innerHeight - document.getElementById('send_message_form').clientHeight - document.getElementById('foo').clientHeight - 10 +'px';
+}
+
+window.addEventListener('resize', message_form_resize);
+document.addEventListener('DOMContentLoaded', message_form_resize);
+
+
+document.addEventListener('DOMContentLoaded',function(){
+  if (!localStorage.getItem('username') && !localStorage.getItem('email')){
+    $('#registration').modal({ backdrop: 'static', keyboard: false })//localStorage.setItem('counter', 0);
+    document.getElementById('username').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('error_with_db_update').classList.add('d-none');
+  }
+  else {
+    document.getElementById('user_label').innerHTML = 'Hi, ' + localStorage.getItem('username') + '!';
+  } 
+});
